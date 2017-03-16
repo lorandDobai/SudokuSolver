193a8ed7b4ad8853d9f2c6a2fd96917b37610527
Lorand Dobai <lorand.dobai@gmail.com>
2017-03-16 09:12:26 +0200

    code clean-up'

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,11 +1,13 @@
 
 ONE_TO_NINE=set(i for i in range(1,10))
 
+
 def collision_coord(i):
         line=[j for j in range((i//9)*9,(i//9+1)*9)]
         col=[j for j in range(i%9,9*9,9) ]
         box=[j for j in boxfinder(i//9,i%9) ]
         return line+col+box
+
 
 def boxfinder(l,c):
     t=[0,0]
@@ -17,6 +19,7 @@ def boxfinder(l,c):
     return [(i*9+j%9) for i in range(t[0][0],t[0][1]) for j in range(t[1][0],t[1][1])]
 
 COLLISION_COORDS=[(collision_coord(i)) for i in range(81)]
+
 
 def read_puzzles_from_pe_file(file):
     m=[]
@@ -33,7 +36,6 @@ class Sudoku(object):
         self.sudoku_puzzle=[]
         self.solution_table=[]
         self.solution_order=[]
-        
 
     def deduction_solve(self):
         change_made = True
@@ -47,7 +49,6 @@ class Sudoku(object):
                     if len(sols) == 1:
                         self.sudoku_puzzle[i] = sols[0]
                         change_made = True
-      
     
     def find_candidates(self):
         self.solution_table=[False]*81
@@ -55,7 +56,6 @@ class Sudoku(object):
             if self.sudoku_puzzle[i] not in ONE_TO_NINE:
                 collision_points=set(self.sudoku_puzzle[j] for j in COLLISION_COORDS[i])
                 self.solution_table[i]=set((filter(lambda x:x not in collision_points,ONE_TO_NINE)))
-
 
     def backtrack_solve(self):
         if not self.solution_table :
@@ -88,6 +88,7 @@ def print_sudoku(s):
         print(str(s[9*i:9*(i+1)]))
     print()
 
+
 def write_solution_to_file(filename,sud,index):
     with open("ResultsFor"+filename,'a') as f:
         f.write("Grid {}\n".format(index))
@@ -98,29 +99,30 @@ def write_solution_to_file(filename,sud,index):
 
 def solve_puzzles(file_name,out):
 
-        try:
-            puzzle_book=read_puzzles_from_pe_file(file_name)
-        except NameError:
-            print("No such file found")
-            return   
-        solved=list()
+    try:
+        puzzle_book=read_puzzles_from_pe_file(file_name)
+    except NameError:
+        print("No such file found")
+        return
+    solved=list()
 
-        for sudoku_puzzles in puzzle_book:
-           
-            solved.append(Sudoku(sudoku_puzzles).solve_sudoku())
-            if out == 'S':
-                print(len(solved))
-                print_sudoku(solved[-1])
-            if out == "F":
-                write_solution_to_file(file_name,solved[-1],len(solved))
+    for sudoku_puzzles in puzzle_book:
+
+        solved.append(Sudoku(sudoku_puzzles).solve_sudoku())
+        if out == 'S':
+            print(len(solved))
+            print_sudoku(solved[-1])
+        if out == "F":
+            write_solution_to_file(file_name,solved[-1],len(solved))
+
 
 def main():
-        from time import clock
-        file_name=input("File:").strip()
-        cout_method=input("Write results to screen or new file(ResultsFor{})?(S/F):".format(file_name)).strip().upper()
-        ht=clock()
-        solve_puzzles(file_name,cout_method.upper())
-        print(clock()-ht)
+    from time import clock
+    file_name=input("File:").strip()
+    cout_method=input("Write results to screen or new file(ResultsFor{})?(S/F):".format(file_name)).strip().upper()
+    ht=clock()
+    solve_puzzles(file_name,cout_method.upper())
+    print(clock()-ht)
     
         
 
